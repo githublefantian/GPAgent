b22c5c55c7f6b8868e826215febe849caf931b2a
wenfengshi <wuzhimang@gmail.com>
2016-12-03 20:07:21 +0800

    update

--- clean.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-source ./agent.env
-
-rm -rf ${TMPPCAP_DIR}/*
-# rm -rf ${RESULT_DIR}/*
-# rm -rf ${LOG_DIR}/*
--- mysetup.py
+++
@@ -1,4 +0,0 @@
-from distutils.core import setup
-import py2exe
-
-setup(console=["http-filter-v2.3.py"])
