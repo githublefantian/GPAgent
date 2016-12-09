f2b9f07cd002e0f0ac88c43d32e53d42e31eddec
wenfengshi <wuzhimang@gmail.com>
2016-12-09 13:32:17 +0800

    update

--- agent/ports_statistics.c
+++ agent/ports_statistics.c
@@ -1,4 +1,5 @@
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <pcap.h>
 #include <arpa/inet.h>
@@ -6,6 +7,14 @@
 //Header sizes for display purposes 
 #define ETHER_HEADERSIZE 14
 #define ARP_HEADERSIZE 28
+#define LINEBUFFER 256
+
+// Ports
+#define PORT1 80
+#define PORT2 443
+#define PORT3 843
+#define PORT4 8300
+
 //Pre-Calculated TCP Header Offset 
 #define TCPHDROFFSET(th)  (((th)->dataoffset & 0xf0) >> 4)
             
@@ -50,21 +59,43 @@ typedef struct TCP
 }TCP;
 
 
-unsigned short getethertype(const unsigned char *);
-void parser(char *);
+unsigned long parser(char *, FILE *, unsigned int);
 
 
 int main(int argc, char *argv[])
 {
+    if(argc != 4) {
+        printf("[%s]parameters missing (4)\n", __FILE__);
+        return 1;
+    }
     //File name is sent as an argument
     char *file = argv[1];
-    
-    //Display the menu and take the user's choice 
-    parser(file);
-    return 0;
+    char *wfile = argv[2];
+    unsigned int period = atoi(argv[3]);
+    FILE *wf = NULL;
+    unsigned long ret = 1;
+
+    if(file == NULL || wfile == NULL) {
+        printf("[%s]main parameters error!\n", __FILE__);
+        return ret;
+    }
+    wf = fopen(wfile, "w");
+    if(wf == NULL) {
+        printf("[%s]Open %s failed!\n", __FILE__, wfile);
+        return ret;
+    } else {
+        ret = parser(file, wf, period);
+        if(ret == 0) {
+            printf("[%s] parser failed!\n", __FILE__);
+            return ret;
+        }
+    }
+
+    return ret;
 } 
 
-void parser(char *file)
+// return the END TIME
+unsigned long parser(char *file, FILE* wf, unsigned int period)
 {
     
     //Create a packet header and a data object
@@ -75,53 +106,107 @@ void parser(char *file)
     TCP *tcp = NULL;
     unsigned short sport = 0,dport = 0;
     unsigned long curtime = 0;
-    
+    unsigned long pretime = 0;
+
     //Variable Declarations
     int ipheadlen = 0;
     int val = 0;
     unsigned long count = 0;
+    unsigned long counter[4] = {0};
+    int len = 0;
 
     //Char array to hold the error. PCAP_ERRBUF_SIZE is defined as 256.
-    char errbuff[PCAP_ERRBUF_SIZE];
+    char errbuff[PCAP_ERRBUF_SIZE] = {0};
+    char wfbuffer[LINEBUFFER] = {0};
 
-    memset(errbuff, 0, PCAP_ERRBUF_SIZE);
- 
+    if(wf == NULL || period < 0) {
+        printf("[%s][%d]parameters error \n", __FILE__, __LINE__);
+        return 0;
+    }
+
     //Open the saved captured file and store result in pointer to pcap_t
     pcap_t *pcap = pcap_open_offline(file, errbuff);
  
-    if (pcap == NULL)
-    {
+    if(pcap == NULL) {
         printf("[%s]pcap_open_offline failed: %s\n", __FILE__, errbuff);
-        return;
+        return 0;
     }
-    //Start reading packets one by one 
-    while (1)
-    {
+    //Start reading packets one by one
+    while(1) {
         val = pcap_next_ex(pcap, &header, &data);
-        if(val < 0) break;
+        if(val < 0) {
+                len = sprintf(wfbuffer, "%lu,%lu,%lu,%lu,%lu\n", pretime, counter[0], counter[1], counter[2], counter[3]);
+                len = fwrite(wfbuffer, sizeof(char), len, wf);
+                len = sprintf(wfbuffer,"\nEND_TIME,%lu\n", curtime);
+                fwrite(wfbuffer, sizeof(char), len, wf);
+            break;
+        }
 
+        curtime = header->ts.tv_sec;
+        if(pretime == 0) {
+            pretime = curtime;
+            len = sprintf(wfbuffer,"START_TIME,%lu\nPORTS,%u,%u,%u,%u\nPERIOD,%u\n\n", pretime, PORT1, PORT2, PORT3, PORT4, period);
+            fwrite(wfbuffer, sizeof(char), len, wf);
+        }
+
+        // IP
+        sport = 0;
+        dport = 0;
         ethernet = (ether*)(data);
-        if(ethernet->type == 8) // TCP/IP
-        {
-            curtime = header->ts.tv_sec;
-
+        if(ethernet->type == 8) {
             //Point to the IP header i.e. 14 bytes(Size of ethernet header) from the start
             ip = (IP*)(data + ETHER_HEADERSIZE);
-            if((unsigned int)(ip->protocol) != 6)
-            {
-                //printf("not tcp protocol\n");
-                continue;
+            // TCP
+            if((unsigned int)(ip->protocol) == 6) {
+                ipheadlen = (ip->headlen & 0x0f)*4;
+                //Point to the TCP header as explained in IP
+                tcp = (TCP*)(data + ETHER_HEADERSIZE + ipheadlen);
+                sport = ntohs(tcp->srcport);
+                dport = ntohs(tcp->destport);
             }
-            ipheadlen = (ip->headlen & 0x0f)*4;
-
-            //Point to the TCP header as explained in IP
-            tcp = (TCP*)(data + ETHER_HEADERSIZE + ipheadlen);
-            sport = ntohs(tcp->srcport);
-            dport = ntohs(tcp->destport);
-            count++;
-            printf("Epoch Time: %lu, srcport: %u, dstport: %u, count: %lu\n", curtime, sport, dport, count);
-            
         }
+        if(curtime >= pretime) {
+            count = (curtime - pretime) / period;
+        } else {
+            continue;
+        }
+        while(1) {
+            if(count == 0) {
+                switch(sport){
+                    case PORT1: counter[0]++; break;
+                    case PORT2: counter[1]++; break;
+                    case PORT3: counter[2]++; break;
+                    case PORT4: counter[3]++; break;
+                    default: {
+                        switch(dport){
+                            case PORT1: counter[0]++; break;
+                            case PORT2: counter[1]++; break;
+                            case PORT3: counter[2]++; break;
+                            case PORT4: counter[3]++; break;
+                            default: break;
+                        }
+                        break;
+                    }
+                }
+                break;
+            } else {
+                len = sprintf(wfbuffer, "%lu,%lu,%lu,%lu,%lu\n", pretime, counter[0], counter[1], counter[2], counter[3]);
+                if(len < 0) {
+                    printf("sprintf error!\n");
+                    return 0;
+                }
+                len = fwrite(wfbuffer, sizeof(char), len, wf);
+                if(len < 0) {
+                    printf("fwrite error!\n");
+                    return 0;
+                }
+                memset(counter, 0, sizeof(counter));
+                pretime = curtime;
+                count--;
+            }
+        }
+
     }
-    return;
+
+    return curtime;
 }
