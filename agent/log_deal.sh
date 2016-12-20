4c4cceed381d9ad79c92bc9927f3d0f5013c63a0
wenfengshi <wuzhimang@gmail.com>
2016-12-20 21:13:35 +0800

    add ip prefix

--- agent/log_deal.sh
+++ agent/log_deal.sh
@@ -5,13 +5,13 @@ source /root/agent/agent.env
 
 for file in `ls ${LOG_DIR}/*.log`; do
     filename=`basename ${file}`
-    [ "${filename:0:2}" == "20" ] && continue
+    [ "${filename:0:2}" == "${IP_PREFIX:0:2}" ] && continue
     year=`date +%Y`
     month=`date +%m`
     let month--
     [ ${month} -eq 0 ] && month=12 && let year--
     [ ${#month} -eq 1 ] && month="0${month}"
-    prefix="${year}${month}_"
+    prefix="${IP_PREFIX}${year}${month}_"
     archive_fn=${prefix}${filename}
     cp ${file} ${file%/*}/${archive_fn}
     [ $? -eq 0 ] && echo "===== archive ${archive_fn} success! =====" > ${file}
